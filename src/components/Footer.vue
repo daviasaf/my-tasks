a000b3d84b3873d842a52bebb07f0ce76bbd0413
Davi Asaf <daviasaf16@gmail.com>
2025-10-18 12:02:58 -0300

    Mudança da cor de link e correção de classes avulsas

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,7 @@
 <template>
   <footer class="bg-green-200  text-sm h-40 flex justify-center items-center">
     <p class="transition-all duration-300 hover:text-base">
-      Criado por <a href="https://github.com/daviasaf" target="_blank" class="transition-all duration-300 hover:text-lg hover:text-emerald-700 hover:text-shadow-amber-400 ">Davi Asaf</a>
+      Criado por <a href="https://github.com/daviasaf" target="_blank" class="transition-all duration-300 hover:text-lg hover:text-emerald-700 ">Davi Asaf</a>
     </p>
   </footer>
 </template>
