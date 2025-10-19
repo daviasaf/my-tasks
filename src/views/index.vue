395fbacdcde93d3862647e6a18ab2d7a1abc4325
Davi Asaf <daviasaf16@gmail.com>
2025-10-19 13:35:27 -0300

    Correção de bug no botão

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,25 +1,22 @@
 <script setup>
 import Button from "../components/Button.vue";
 import UserComment from "../components/UserComment.vue";
-import pessoa1 from '../assets/pessoa1.jpg'
-import pessoa2 from '../assets/pessoa2.jpg'
-import pessoa3 from '../assets/pessoa3.jpg'
-
-
+import pessoa1 from "../assets/pessoa1.jpg";
+import pessoa2 from "../assets/pessoa2.jpg";
+import pessoa3 from "../assets/pessoa3.jpg";
 </script>
 
 <template>
-
   <!-- APRESENTAÇÃO DA P -->
   <div class="grid gap-4 grid-cols-1 mx-4 my-20">
     <h1 class="text-5xl text-center uppe">My Tasks</h1>
     <p>Salve suas tarefas em um único lugar!</p>
-    <Button #buttonContent
-      ><routerLink to="/tasks">Comece agora</routerLink></Button
-    >
+    <routerLink to="/tasks">
+      <Button #buttonContent>Comece agora!</Button>
+    </routerLink>
   </div>
 
-<!-- COMENTÁRIOS -->
+  <!-- COMENTÁRIOS -->
   <div class="flex flex-wrap justify-center my-10">
     <UserComment :url="pessoa2">
       <template #nome>Jonas</template>
@@ -29,15 +26,11 @@ import pessoa3 from '../assets/pessoa3.jpg'
     </UserComment>
     <UserComment :url="pessoa3">
       <template #nome>Carmen</template>
-      <template #comentario>
-        Incrível!
-      </template>
+      <template #comentario> Incrível! </template>
     </UserComment>
     <UserComment :url="pessoa1">
       <template #nome>Francis</template>
-      <template #comentario>
-        Simples e funcional!
-      </template>
+      <template #comentario> Simples e funcional! </template>
     </UserComment>
   </div>
 </template>
